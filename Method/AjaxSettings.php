9f1adfdc33f63c838dd6860a7c46bb1471ac72b7
gizmore <gizmore@wechall.net>
2022-08-27 10:13:35 +0200

    '7.0.1-r1538'

---
+++ Method/AjaxSettings.php
@@ -0,0 +1,49 @@
+<?php
+namespace GDO\Account\Method;
+
+use GDO\Core\MethodAjax;
+use GDO\Core\ModuleLoader;
+use GDO\User\GDO_User;
+use GDO\Core\GDT_JSON;
+
+/**
+ * Get all user settings for the current user via ajax.
+ * 
+ * @author gizmore
+ * @version 7.0.1
+ * @since 6.7.0
+ */
+final class AjaxSettings extends MethodAjax
+{
+	public function isUserRequired() : bool
+	{
+		return true;
+	}
+	
+	public function execute()
+	{
+		$user = GDO_User::current();
+		$json = ['user' => $user->toJSON()];
+		$modules = ModuleLoader::instance()->getEnabledModules();
+		foreach ($modules as $module)
+		{
+			$modulename = $module->getName();
+			
+			foreach ($module->getSettingsCache() as $gdt)
+			{
+				$gdt = $module->userSetting($user, $gdt->name); # to assign current user to gdt
+				
+				if ($gdt->isSerializable())
+				{
+					if (!isset($json[$modulename]))
+					{
+						$json[$modulename] = [];
+					}
+					$json[$modulename][$gdt->name] = $gdt->configJSON();
+				}
+			}
+		}
+		return GDT_JSON::make()->value($json);
+	}
+
+}
