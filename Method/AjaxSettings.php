c07beb1416eb474fcdaeaa17748aa5cea4aea5ae
gizmore <gizmore@wechall.net>
2023-05-20 15:12:50 +0200

    '7.0.3-r1792'

--- Method/AjaxSettings.php
+++ Method/AjaxSettings.php
@@ -34,7 +34,7 @@ final class AjaxSettings extends MethodAjax
 			foreach ($module->getSettingsCache() as $gdt)
 			{
 				$gdt = $module->userSetting($user, $gdt->getName()); # to assign current user to gdt
-				if ($gdt->isSerializable() && $gdt->isACLCapable())
+				if ($gdt->isSerializable() && $gdt->isACLCapable() && (!$gdt->isHidden()))
 				{
 					$json[$modulename] = $json[$modulename] ?? [];
 					$json[$modulename][$gdt->getName()] = [
